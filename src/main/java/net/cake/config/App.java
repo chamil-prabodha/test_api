5ed0dee48e63ef706822d2b70291508c3669d5da
chamilp <chamil.prabodha@trycake.com>
2018-02-21 16:14:48 +0530

    add separate database config

--- src/main/java/net/cake/config/App.java
+++ src/main/java/net/cake/config/App.java
@@ -16,7 +16,7 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @PropertySource("classpath:application.properties")
 @EnableTransactionManagement
 @EnableJpaRepositories("net.cake.dao")
-@EntityScan(basePackages = "net.cake.model")
+@EntityScan(basePackages = "net.cake.model.datamodel")
 public class App {
   public static void main(String[] args) throws Exception {
     SpringApplication.run(App.class, args);
--- src/main/java/net/cake/config/PersistenceJpaConfig.java
+++ src/main/java/net/cake/config/PersistenceJpaConfig.java
@@ -24,73 +24,73 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 /**
  * Created by chamilp on 2/20/18.
  */
-//@Configuration
+@Configuration
 @EnableTransactionManagement
 @ComponentScan("net.cake")
 @EnableJpaRepositories("net.cake.dao")
-@PropertySource(value = {"classpath:db.properties"})
+@PropertySource("classpath:db.properties")
 public class PersistenceJpaConfig {
 
   @Autowired
   private Environment environment;
 
-//  @Bean
-//  public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
-//    LocalContainerEntityManagerFactoryBean em = new LocalContainerEntityManagerFactoryBean();
-//    em.setDataSource(hikariDataSource());
-//    em.setPackagesToScan("net.cake.model");
-//
-//    JpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
-//    em.setJpaVendorAdapter(vendorAdapter);
-//    em.setJpaProperties(additionalProperties());
-//
-//    return em;
-//  }
+  @Bean
+  public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
+    LocalContainerEntityManagerFactoryBean em = new LocalContainerEntityManagerFactoryBean();
+    em.setDataSource(hikariDataSource());
+    em.setPackagesToScan("net.cake.model");
 
-//  @Bean
-//  public DataSource hikariDataSource() {
-//    final HikariDataSource ds = new HikariDataSource();
-//    ds.setMaximumPoolSize(20);
-//    ds.setDataSourceClassName(environment.getRequiredProperty("jdbc.dataSourceClassName"));
-//    ds.addDataSourceProperty("url", environment.getRequiredProperty("jdbc.url"));
-//    ds.addDataSourceProperty("user", environment.getRequiredProperty("jdbc.username"));
-//    ds.addDataSourceProperty("password", environment.getRequiredProperty("jdbc.password"));
-//    ds.addDataSourceProperty(
-//        "cachePrepStmts", environment.getRequiredProperty("hikari.cache_prep_statement"));
-//    ds.addDataSourceProperty(
-//        "prepStmtCacheSize", environment.getRequiredProperty("hikari.prep_stmt_cache_size"));
-//    ds.addDataSourceProperty(
-//        "prepStmtCacheSqlLimit",
-//        environment.getRequiredProperty("hikari.prep_stmt_cache_sql_limit"));
-//    ds.addDataSourceProperty(
-//        "useServerPrepStmts", environment.getRequiredProperty("hikari.use_server_prep_stmts"));
-//    return ds;
-//  }
+    JpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
+    em.setJpaVendorAdapter(vendorAdapter);
+    em.setJpaProperties(additionalProperties());
 
-//  @Bean
-//  public PlatformTransactionManager transactionManager(EntityManagerFactory entityManagerFactory) {
-//    JpaTransactionManager transactionManager = new JpaTransactionManager();
-//    JpaDialect jpaDialect = new HibernateJpaDialect();
-//    transactionManager.setEntityManagerFactory(entityManagerFactory);
-//    transactionManager.setJpaDialect(jpaDialect);
-//    return transactionManager;
-//  }
-//
-//  @Bean
-//  public PersistenceExceptionTranslationPostProcessor exceptionTranslation() {
-//    return new PersistenceExceptionTranslationPostProcessor();
-//  }
-//
-//  Properties additionalProperties() {
-//    Properties properties = new Properties();
-//    properties.setProperty(
-//        "hibernate.hbm2ddl.auto", environment.getRequiredProperty("hibernate.orm_generation"));
-//    properties.setProperty(
-//        "hibernate.dialect", environment.getRequiredProperty("hibernate.dialect"));
-//    properties.setProperty(
-//        "hibernate.show_sql", environment.getRequiredProperty("hibernate.show_sql"));
-//    properties.setProperty(
-//        "hibernate.format_sql", environment.getRequiredProperty("hibernate.format_sql"));
-//    return properties;
-//  }
+    return em;
+  }
+
+  @Bean
+  public DataSource hikariDataSource() {
+    final HikariDataSource ds = new HikariDataSource();
+    ds.setMaximumPoolSize(20);
+    ds.setDataSourceClassName(environment.getRequiredProperty("jdbc.dataSourceClassName"));
+    ds.addDataSourceProperty("url", environment.getRequiredProperty("jdbc.url"));
+    ds.addDataSourceProperty("user", environment.getRequiredProperty("jdbc.username"));
+    ds.addDataSourceProperty("password", environment.getRequiredProperty("jdbc.password"));
+    ds.addDataSourceProperty(
+        "cachePrepStmts", environment.getRequiredProperty("hikari.cache_prep_statement"));
+    ds.addDataSourceProperty(
+        "prepStmtCacheSize", environment.getRequiredProperty("hikari.prep_stmt_cache_size"));
+    ds.addDataSourceProperty(
+        "prepStmtCacheSqlLimit",
+        environment.getRequiredProperty("hikari.prep_stmt_cache_sql_limit"));
+    ds.addDataSourceProperty(
+        "useServerPrepStmts", environment.getRequiredProperty("hikari.use_server_prep_stmts"));
+    return ds;
+  }
+
+  @Bean
+  public PlatformTransactionManager transactionManager(EntityManagerFactory entityManagerFactory) {
+    JpaTransactionManager transactionManager = new JpaTransactionManager();
+    JpaDialect jpaDialect = new HibernateJpaDialect();
+    transactionManager.setEntityManagerFactory(entityManagerFactory);
+    transactionManager.setJpaDialect(jpaDialect);
+    return transactionManager;
+  }
+
+  @Bean
+  public PersistenceExceptionTranslationPostProcessor exceptionTranslation() {
+    return new PersistenceExceptionTranslationPostProcessor();
+  }
+
+  Properties additionalProperties() {
+    Properties properties = new Properties();
+    properties.setProperty(
+        "hibernate.hbm2ddl.auto", environment.getRequiredProperty("hibernate.orm_generation"));
+    properties.setProperty(
+        "hibernate.dialect", environment.getRequiredProperty("hibernate.dialect"));
+    properties.setProperty(
+        "hibernate.show_sql", environment.getRequiredProperty("hibernate.show_sql"));
+    properties.setProperty(
+        "hibernate.format_sql", environment.getRequiredProperty("hibernate.format_sql"));
+    return properties;
+  }
 }
